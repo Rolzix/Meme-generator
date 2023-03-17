1546e3c3b7944540ec56a469821d64ca5a32cb31
Rolzi <aki.peltoniemi@gmail.com>
2023-03-17 21:08:27 +0200

    Get random image url function

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import data from "../memesData";
 
 export default function Meme() {
+  function getImage() {
+    let rand = Math.floor(Math.random() * data.data.memes.length);
+    console.log(data.data.memes[rand].url);
+    url = data.data.memes[rand].url;
+  }
   return (
     <main>
-      <form className="inputForm">
+      <div className="inputForm">
         <input className="upperInput" type="text" placeholder="Top text" />
         <input className="lowerInput" type="text" placeholder="Bottom text" />
-        <button className="newImage">
+        <button onClick={getImage} className="newImage">
           Get a new meme image 🖼<img></img>
         </button>
-      </form>
+      </div>
     </main>
   );
 }
