3ea4e0b05e2a4346a7db96dee24c0c6a6fd9ab9f
Rolzi <aki.peltoniemi@gmail.com>
2023-03-17 20:15:17 +0200

    Base header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="nav--main">
+      <img className="nav--image" src="/src/images/Troll Face.png" alt="" />
+      <h1>Meme Generator</h1>
+    </div>
+  );
+}
