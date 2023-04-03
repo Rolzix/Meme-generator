58530d5f7d56f061d5058ec3a93df611df527baa
Rolzi <aki.peltoniemi@gmail.com>
2023-04-03 19:23:09 +0300

    Made meme an object and added an allMemeImages state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,24 +2,35 @@ import React from "react";
 import data from "../memesData";
 
 /**
- * Challenge: Save the random meme URL in state
- * - Create new state called `memeImage` with an
- *   empty string as default
- * - When the getMemeImage function is called, update
- *   the `memeImage` state to be the random chosen
- *   image URL
- * - Below the div.form, add an <img /> and set the
- *   src to the new `memeImage` state you created
+ * Challenge: Update our state to save the meme-related
+ * data as an object called `meme`. It should have the
+ * following 3 properties:
+ * topText, bottomText, randomImage.
+ *
+ * The 2 text states can default to empty strings for now,
+ * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+ *
+ * Next, create a new state variable called `allMemeImages`
+ * which will default to `memesData`, which we imported above
+ *
+ * Lastly, update the `getMemeImage` function and the markup
+ * to reflect our newly reformed state object and array in the
+ * correct way.
  */
-
+s;
 export default function Meme() {
-  let [memeImage, setMemeImage] = React.useState("");
-  // console.log(React.useState(""));
+  let [meme, setMeme] = React.useState({
+    TopText: "",
+    BottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  let [allMemeImages, setAllMemeImages] = React.useState(data.data.memes);
+  // console.log(allMemeImages);
   function getImage() {
-    let rand = Math.floor(Math.random() * data.data.memes.length);
-    // console.log(data.data.memes[rand].url);
-    let url = data.data.memes[rand].url;
-    setMemeImage(url);
+    let rand = Math.floor(Math.random() * allMemeImages.length);
+    console.log(allMemeImages.length);
+    let url = allMemeImages[rand].url;
+    setMeme({ ...meme, randomImage: url });
   }
   return (
     <main>
@@ -30,7 +41,7 @@ export default function Meme() {
           Get a new meme image 🖼
           <br />
         </button>
-        <img src={memeImage} className="memeImage"></img>
+        <img src={meme.randomImage} className="memeImage"></img>
       </div>
     </main>
   );
