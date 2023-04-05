54b51997dd2a43d1901badbb269a9ba73d261adb
Rolzi <aki.peltoniemi@gmail.com>
2023-04-05 17:22:31 +0300

    Text added to state. Input fiel set to update text on change.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,31 +1,19 @@
 import React from "react";
 import data from "../memesData";
 
-/**
- * Challenge: Update our state to save the meme-related
- * data as an object called `meme`. It should have the
- * following 3 properties:
- * topText, bottomText, randomImage.
- *
- * The 2 text states can default to empty strings for now,
- * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
- *
- * Next, create a new state variable called `allMemeImages`
- * which will default to `memesData`, which we imported above
- *
- * Lastly, update the `getMemeImage` function and the markup
- * to reflect our newly reformed state object and array in the
- * correct way.
- */
-s;
 export default function Meme() {
   let [meme, setMeme] = React.useState({
-    TopText: "",
-    BottomText: "",
+    TopText: "One does not simply",
+    BottomText: "Walk into Mordor",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
   let [allMemeImages, setAllMemeImages] = React.useState(data.data.memes);
   // console.log(allMemeImages);
+  function updateText() {
+    let { name, value } = event.target;
+    setMeme({ ...meme, [name]: value });
+  }
+
   function getImage() {
     let rand = Math.floor(Math.random() * allMemeImages.length);
     console.log(allMemeImages.length);
@@ -35,13 +23,29 @@ export default function Meme() {
   return (
     <main>
       <div className="inputForm">
-        <input className="upperInput" type="text" placeholder="Top text" />
-        <input className="lowerInput" type="text" placeholder="Bottom text" />
+        <input
+          className="upperInput"
+          type="text"
+          placeholder="Top text"
+          name="TopText"
+          onChange={updateText}
+        />
+        <input
+          className="lowerInput"
+          type="text"
+          placeholder="Bottom text"
+          name="BottomText"
+          onChange={updateText}
+        />
         <button onClick={getImage} className="newImage">
           Get a new meme image 🖼
           <br />
         </button>
-        <img src={meme.randomImage} className="memeImage"></img>
+        <div className="meme">
+          <img src={meme.randomImage} className="meme--image" />
+          <h2 className="meme--text top">{meme.TopText}</h2>
+          <h2 className="meme--text bottom">{meme.BottomText}</h2>
+        </div>
       </div>
     </main>
   );
