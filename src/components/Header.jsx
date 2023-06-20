a5b20c489017dcb79283c1a2188753b848b1e185
Rolzi <aki.peltoniemi@gmail.com>
2023-06-20 19:42:13 +0300

    Changed logo to brain.
    Added media query for logo reposition on small screens.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div className="nav--main">
-      <img className="nav--image" src="/src/images/Troll Face.png" alt="" />
+      <img className="nav--image" src="/src/images/brain.jpg" alt="" />
       <h1>Meme Generator</h1>
     </div>
   );
