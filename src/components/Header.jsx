9e330a0e23e39f0bf422bac19bbb50d85e720161
Rolzi <aki.peltoniemi@gmail.com>
2023-07-04 21:05:18 +0300

    Proper image import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import imgUrl from "/src/assets/brain.jpg";
 export default function Header() {
   return (
     <div className="nav--main">
-      <img className="nav--image" src="/src/assets/brain.jpg" alt="" />
+      <img className="nav--image" src={imgUrl} alt="" />
       <h1>Meme Generator</h1>
     </div>
   );
